f68bb618a247bfefa780b8d5a5120e6822675aae
David Smith <davidssmith93@gmail.com>
2022-04-07 02:18:08 +0100

    Update webview.js

--- webview.js
+++ webview.js
@@ -1,18 +1,5 @@
 import path from 'path';
 
 module.exports = (Franz) => {
-  const getMessages = function getMessages() {
-    // get unread messages
-    const direct = document.querySelectorAll('[class^="guildsWrapper"] [class*="badge"]').length;
-    const indirect = document.querySelectorAll('[class^="guildsWrapper"] [class^="guild-"]+[class*="unread-"]').length;
 
-    // set Franz badge
-    Franz.setBadge(direct, indirect);
-  };
-
-  // check for new messages every second and update Franz badge
-  Franz.loop(getMessages);
-
-  // Hide download message
-  Franz.injectCSS(path.join(__dirname, 'service.css'));
 };
